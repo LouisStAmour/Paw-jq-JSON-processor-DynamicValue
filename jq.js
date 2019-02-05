c4b4c80cbb098b242c8c564d6214f2c313fe6252
Louis St-Amour <LouisStAmour@gmail.com>
2019-02-05 01:29:57 -0500

    Correcting jq raw behaviour to strip shell newline

--- jq.js
+++ jq.js
@@ -1,22 +1,20 @@
 this.jq = this.require("./jq-emscripten") || this.jq;
 
 var ProcessWithJQ = function() {
-  var flags = '-M';
+  var flags = "-M";
 
   this.evaluate = function() {
-    if (this.rawOutput)
-      flags += 'r';
+    if (this.rawOutput) flags += "r";
 
     var jqOutput = jq.raw(this.jsonInput, [flags, this.jqargs]);
 
-    if (jqOutput === null)
-      return null;
+    if (jqOutput === null) return null;
 
     if (jqOutput.match(/^jq: error:/)) {
       console.error(jqOutput);
       return null;
     } else {
-      return jqOutput;
+      return jqOutput.replace(/\n$/g, "");
     }
   };
   this.title = function() {
@@ -32,7 +30,9 @@ ProcessWithJQ.title = "jq JSON processor";
 ProcessWithJQ.help = "https://stedolan.github.io/jq/manual/#Basicfilters";
 ProcessWithJQ.inputs = [
   InputField("jqargs", "JQ args", "String", { defaultValue: "." }),
-  InputField("jsonInput", "JSON input", "String", { placeholder: "{\"change me\": \"put your JSON input here\"}" }),
+  InputField("jsonInput", "JSON input", "String", {
+    placeholder: '{"change me": "put your JSON input here"}'
+  }),
   InputField("rawOutput", "Raw output", "Checkbox", { defaultValue: false })
 ];
 registerDynamicValueClass(ProcessWithJQ);
